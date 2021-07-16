bccf2d796c74d83737b857abb1297bf63491d625
BENJAMIN IDEWOR <benjamin.idewor@decagon.dev>
2021-07-16 21:57:20 +0100

    set host

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6nc@$zcd$^*r_4m^d!#brs&v%sg_(&1zia&d*-#p+a0$&26@jz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ["192.168.43.253"]
 
 LOGIN_REDIRECT_URL = 'users:dashboard'
