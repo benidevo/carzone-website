67c5882f1fc4c22af9f829e59e52795ba0006e74
Benjamin Idewor <benjaminidewor@gmail.com>
2021-12-11 21:42:31 +0100

    bgfix: update email server

--- carzone/settings.py
+++ carzone/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -186,8 +188,8 @@ LOGOUT_REDIRECT_URL = '/'
 # send emails
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'benidevoo@gmail.com'
-EMAIL_HOST_PASSWORD = 'dvteylngponlarxe'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
